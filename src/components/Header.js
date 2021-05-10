75ecad6e1a9a20887ec7cc45abe137c0749d2af0
Kavya <bajajkavya48@gmail.com>
2021-05-10 05:36:05 +0530

    Expense tracker

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2>
+            Keep track of all your expenses !!
+        </h2>
+    )
+}
---
+++ src/components/Transaction.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import {GlobalContext} from '../Context/GlobalState';
+
+export const Transaction = ({transaction}) => {
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const { deleteTransaction } = useContext(GlobalContext);
+
+    return (
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text}
+            <span>{sign} {Math.abs(transaction.amount)} INR</span>
+            <button className='delete-btn' 
+            onClick = {() => deleteTransaction(transaction.id)}>x</button>
+        </li>
+    )
+}
